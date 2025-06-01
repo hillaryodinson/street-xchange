0b8b73875707530f04b0dc90986ee0b507a7ad7f
Hillary Odinchefu <insanedude.developer@gmail.com>
2025-06-01 09:19:41 +0100

    feat: configure server to use PORT environment variable for dynamic port assignment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,13 @@
 import { initApp } from "./app";
 import { populateDB } from "./configs/db";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { PORT } = process.env;
+if (!PORT) {
+	throw new Error("PORT environment variable is not set");
+}
 
 const app = initApp();
 populateDB();
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log("Server running on port " + PORT));

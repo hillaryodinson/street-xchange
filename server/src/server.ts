da23e635dacc78ebff0f1eab53a1df897fe158b8
Hillary Odinchefu <insanedude.developer@gmail.com>
2025-07-06 00:25:02 +0100

    feat: simplify PORT initialization and add default value

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,10 @@ import { populateDB } from "./configs/db";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { PORT } = process.env;
-if (!PORT) {
-	throw new Error("PORT environment variable is not set");
-}
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 const app = initApp();
 populateDB();
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, "0.0.0.0", () =>
+	console.log("Server running on port " + PORT)
+);
